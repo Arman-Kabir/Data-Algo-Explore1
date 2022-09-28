35caaf0c31ef2dc03bfe49d1e799ec965786dc6f
Arman-Kabir <armankabir8@gmail.com>
2022-09-28 12:43:16 +0600

    insert done

--- 8.11.js
+++ 8.11.js
@@ -37,10 +37,10 @@ class LinkedList {
         return this;
     }
 
-    printList(){
+    printList() {
         const array = [];
         let currentNode = this.head;
-        while(currentNode !== null){
+        while (currentNode !== null) {
             array.push(currentNode.value);
             currentNode = currentNode.next;
         }
@@ -48,7 +48,32 @@ class LinkedList {
     }
 
     insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
 
+        const newNode = {
+            value: value,
+            next: null
+        };
+
+        const leader = this.traverseToIndex(index - 1)
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this.printList();
+
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
     }
 }
 
@@ -57,8 +82,12 @@ const myLinkedList = new LinkedList(50);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-myLinkedList.prepend(10);
+// myLinkedList.prepend(10);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 199);
 
 console.log(myLinkedList.printList());
+
+
 
 console.log(myLinkedList);
