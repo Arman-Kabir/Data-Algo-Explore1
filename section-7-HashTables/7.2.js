4996930cefd38b5fb7343f415033945d104131d9
Arman-Kabir <armankabir8@gmail.com>
2023-01-14 01:47:25 +0600

    hash function intro

---
+++ section-7-HashTables/7.2.js
@@ -0,0 +1,9 @@
+// md5 hash Generator -- SHA-
+// 256 hash generator
+// Hash Function is used all across computer science - a hash function is simply a function
+//  that generates a value of fixed length for each input that it gets
+
+// We give it an input to hash function - and the function generates some random pattern.
+
+// we have a key that is - grapes. We send it through a hash function that is going to
+//  hash something really fast and map 
