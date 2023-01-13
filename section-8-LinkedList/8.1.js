c56a7e5ea61e1aca28db68b284beb8ec6ddf3a30
Arman-Kabir <armankabir8@gmail.com>
2023-01-13 14:17:20 +0600

    linked lists intro

---
+++ section-8-LinkedList/8.1.js
@@ -0,0 +1,19 @@
+// Singly and doubly linked lists
+
+/*  Q. What problem do we encounter  with arrays ? 
+    A. With static arrays - We only had a certain amount of data or memory that can be
+    allocated next to each other in memory. Then both dynamic arrays and static arrays 
+    can increase their memory.---Once it hits a certain limit and double up that memory
+    in another location but that operation every once in a while of doubling up the
+    array in order to create more memory has a performance implication and it costs us
+    all O(n) .Additionally arrays also had bad performance for any sort 
+    of operations like insert and delete that had to shift indexes over especially
+    when u inserted or deleted any word that wasn't the end of the array.
+
+    Then HashTables came. But the data wasn't in ordered.
+
+    Then Linked Lists came to the rescue
+
+
+
+*/
