1e3fbb2f5fbf480aaf5061df39b320e7d2528b11
Arman-Kabir <armankabir8@gmail.com>
2023-01-14 01:35:35 +0600

    hash table intro

---
+++ section-7-HashTables/7.1.js
@@ -0,0 +1,27 @@
+// Hash Tables or hash maps, maps, on ordered maps, dictionaries, objects 
+// Many ways to call this data structure in different languages and slight variations.
+// Objects in js are a type of hash table
+
+// arrays and objects or hash tables -- these 2 are the most common intervierw questions. U r gonna 
+// use them in any coding question.
+
+// We see HashTables a lot in databases, in caches
+
+//*** */ basket.grapes = 10000. Object-basket, grapes has the property of basket object
+// In arrays - We have an index thats numbered and a value
+// In hash table or object - we get to set a key which is grapes and a value.(key-value pair)-->
+// We have a key which is grapes and
+//  this key is used as the index of where to find the value in memory
+
+
+// *** Hash Function
+// It stores both key and value
+// Hash Function decides where to put the data on our memory in our computers.
+
+
+
+
+
+
+
+
