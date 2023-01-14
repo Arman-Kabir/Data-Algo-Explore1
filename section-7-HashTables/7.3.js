87af290cd9ef2135fd8bc70dbb6fdef916e668bd
Arman-Kabir <armankabir8@gmail.com>
2023-01-15 05:31:58 +0600

    7.3 done

---
+++ section-7-HashTables/7.3.js
@@ -0,0 +1,28 @@
+// Insert - O(1), Lookup - O(1), Delete - O(1), Search - O(1)
+
+let user = {
+    age: 54,
+    name: 'Kylie',
+    magic: true,
+    scream: function () {
+        console.log('ahhhhh');
+    }
+}
+
+// age,name,magic,and function scream are all going to get placed somewhere in memory in different
+// addresses. But we can access them really really fast. 
+
+user.age //O(1)
+user.spell = 'halum halum' //O(1)
+console.log(user.scream);
+console.log(user);
+
+// Our computer has limited space and when we create an object or hash table the computer decides
+// how much space to allocate
+
+// COLLISION -- when u have a collission it slows down reading and writing with hash table with O(n)
+// and divided by k where k is the size of your hash table.-- O(n/k).
+// Coalition will happen in every hash table implementation.
+// Two ways to deal with these collissions.
+
+
