2524ced4f4daeb7cb556c83c931e1d9f9f428cba
Arman-Kabir <armankabir8@gmail.com>
2023-01-15 05:59:23 +0600

    7.4 half done

---
+++ section-7-HashTables/7.4.js
@@ -0,0 +1,13 @@
+// Hash tables are implemented differently in different languages but most of the time ----
+// the key and the value can be any type of Data Structure .
+// We can have a function as a value, another object, an array as well. We can have arrays of objects
+
+// In Js in an object --- if u pass a key that is a number or a function it actually
+// gets a string. It only allows string keys.
+
+/*
+But New version of Js and Es6, we have something called Map and Sets.
+
+*/
+
+
