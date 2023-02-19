08bd9ea84e19200b1428957679bacb5936cab1cf
Arman-Kabir <armankabir8@gmail.com>
2023-02-19 18:43:53 +0600

    graphs intro done

---
+++ section-11-Graphs/11.1-5.js
@@ -0,0 +1,8 @@
+// 11.1 Graphs Intro
+/*
+    Graphs are one of the most useful and most used data structure in computer science
+    when it comes to modeling real life.
+    *Graphs is simply a set of values that are related in a pair wise fashion.
+
+    * In a graph - each item is called a node or a vertex. Nodes are then connected with edges.
+*/
