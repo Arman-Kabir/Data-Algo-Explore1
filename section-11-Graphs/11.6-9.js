5778678086baed61aa62ef35be39841664fe87a4
Arman-Kabir <armankabir8@gmail.com>
2023-02-21 09:53:16 +0600

    graphs edges done

---
+++ section-11-Graphs/11.6-9.js
@@ -0,0 +1,26 @@
+
+// Graph 
+class Graph {
+    constructor() {
+        this.numberOfNodes = 0;
+        this.adjacentList = {
+
+        };
+    }
+
+    addVertex(node) {
+        this.adjacentList[node] = [];
+        this.numberOfNodes++;
+    }
+}
+
+const myGraph = new Graph();
+console.log(myGraph);
+myGraph.addVertex('0');
+myGraph.addVertex('1');
+myGraph.addVertex('2');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
+console.log(myGraph);
