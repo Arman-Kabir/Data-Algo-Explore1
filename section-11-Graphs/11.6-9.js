75a472d80f47b884b8f125c4749f76b49688fd16
Arman-Kabir <armankabir8@gmail.com>
2023-02-21 09:58:10 +0600

    graphs  done

--- section-11-Graphs/11.6-9.js
+++ section-11-Graphs/11.6-9.js
@@ -1,5 +1,5 @@
 
-// Graph 
+// Graph 11.6
 class Graph {
     constructor() {
         this.numberOfNodes = 0;
@@ -12,6 +12,11 @@ class Graph {
         this.adjacentList[node] = [];
         this.numberOfNodes++;
     }
+    addEdge(node1, node2) {
+        // Undirected Graph
+        this.adjacentList[node1].push(node2); 
+        this.adjacentList[node2].push(node1); 
+    }
 }
 
 const myGraph = new Graph();
@@ -23,4 +28,6 @@ myGraph.addVertex('3');
 myGraph.addVertex('4');
 myGraph.addVertex('5');
 myGraph.addVertex('6');
+
+myGraph.addEdge('1','3');
 console.log(myGraph);
