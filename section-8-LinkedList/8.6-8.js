69ce1c32163a37f59ac0f6fb5e3f7bdb6f60c64f
Arman-Kabir <armankabir8@gmail.com>
2023-03-08 09:57:09 +0600

    node class

--- section-8-LinkedList/8.6-8.js
+++ section-8-LinkedList/8.6-8.js
@@ -14,6 +14,14 @@
 // }
 
 // Constructor is the function that gets run at the very beginning when we create the linked list
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+// console.log(object);
 class LinkedList{
     constructor(value){
         this.head={
@@ -25,10 +33,11 @@ class LinkedList{
     }
     append(value){
         // code here ---7
-        const newNode={
-            value:value,
-            next:null
-        };
+        // const newNode={
+        //     value:value,
+        //     next:null
+        // };
+        const newNode = new Node(value);
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
@@ -36,10 +45,11 @@ class LinkedList{
     }
     prepend(value){
         // code here---8
-        const newNode={
-            value:value,
-            next:null
-        };
+        // const newNode={
+        //     value:value,
+        //     next:null
+        // };
+        const newNode = new Node(value);
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
