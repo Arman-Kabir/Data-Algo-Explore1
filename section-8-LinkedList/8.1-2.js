d38bcba87b654fb61c634090d7499a08bad5bf8a
Arman-Kabir <armankabir8@gmail.com>
2023-03-08 07:11:59 +0600

    4 partly done

--- section-8-LinkedList/8.1-2.js
+++ section-8-LinkedList/8.1-2.js
@@ -1,12 +1,12 @@
 //1   // Singly and doubly linked lists
 
-/*  Q. What problem do we encounter  with arrays ? 
+/*  Q. What problem do we encounter  with arrays ?
     A. With static arrays - We only had a certain amount of data or memory that can be
-    allocated next to each other in memory. Then both dynamic arrays and static arrays 
+    allocated next to each other in memory. Then both dynamic arrays and static arrays
     can increase their memory.---Once it hits a certain limit and double up that memory
     in another location but that operation every once in a while of doubling up the
     array in order to create more memory has a performance implication and it costs us
-    all O(n) .Additionally arrays also had bad performance for any sort 
+    all O(n) .Additionally arrays also had bad performance for any sort
     of operations like insert and delete that had to shift indexes over especially
     when u inserted or deleted any word that wasn't the end of the array.
 
@@ -26,4 +26,26 @@
     * 
     * Linked list  -- an element that links to the next element that links to the next element and it keeps going, keeps going untill the last element that points to null. u can have any sort of data type.
 
+    const basket = ['apples','grapes','pears'];
+    linked list : apples--> grapes--> pears
+
+    apples
+    8947 --> grapes
+                8742 --> pears
+                        372 --> null
+*/
+
+// 3-4
+/*
+    the main diff between arrays and linked lists is that in an array an element or elements are indexed,
+    in a linked list u start at the head and traverse the list until u get to item
+    *array items are always located right next to each other in computer memory, linked list or nodes instead are scattered all over memory.
+    *Kind of like hash tables so iterating through a linked list or traversing through a linked list is usually quite a bit slower than iterating through items like an array
+    * However inserts that we can do in middle of a linked list is a lot better than an array
+    * 
+*/
+
+//5 What is a pointer?
+/*
+    
 */
