d1a5c741ccaae3698d4e1d41aa084b504e5272bf
Arman-Kabir <armankabir8@gmail.com>
2023-03-20 08:54:52 +0600

    append done

--- section-8-LinkedList/L-List-p1.js
+++ section-8-LinkedList/L-List-p1.js
@@ -9,8 +9,27 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+
+    append(value) {
+        // code here
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value){
+        
+    }
 }
 
 const myLinkedList = new LinkedList(10);
+
+console.log(myLinkedList.append(16));
+console.log(myLinkedList.append(1));
 
 console.log(myLinkedList);
